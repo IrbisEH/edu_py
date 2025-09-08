6c227f3da8674920adc96f9fc52a9f403480b4f1
ehristenko <hristenko.evgeniy@yandex.ru>
2025-09-08 17:34:27 +0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	lutz_python/chapter_32/slots/testing.py

--- lutz_python/chapter_32/slots/testing.py
+++ lutz_python/chapter_32/slots/testing.py
@@ -17,3 +17,37 @@ class C:
     __slots__ = ['a']
     # a = 99        ValueError: 'a' in __slots__ conflicts with class variable
     b = 99
+
+import timeit
+
+base = """
+Is = []
+for i in range(1000):
+    x = C()
+    x.a = 1
+    x.b = 2
+    x.c = 3
+    x.d = 4
+    t = x.a + x.b + x.c + x.d
+    Is.append(t)
+"""
+
+stmt_1 = """
+class C():
+    pass
+""" + base
+
+
+stmt_2 = """
+class C():
+    __slots__ = ['a', 'b', 'c', 'd']
+""" + base
+
+items = [('NonSlots', stmt_1), ('Slots', stmt_2)]
+
+for title, stmt in items:
+    print(f'{title} =>',  end=' ')
+    print(min(timeit.repeat(stmt, number=1000, repeat=3)))
+
+
+
